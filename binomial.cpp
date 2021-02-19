81f0352793d66359f7033c7dea4a56a22398a813
Yuushan <Alexis_rivera@outlook.com>
2021-02-19 00:58:54 +0000

    Stage 1 completion...
    Incomplete

--- binomial.cpp
+++ binomial.cpp
@@ -1,26 +1,95 @@
 #include "binomial.h"
-using namespace N;
+
 //file binomial.cpp
-
-float GetCoefficient(int x){
-   if (x == 1){
-    return float (Binomial::Coefficient1);
-   } 
-    
-   else if (x == 2){
-    return ::Coefficient2;
-   } 
-   else{
-     return -1;
-   }
-
-  }
-
+/*
+Binomial::Binomial(float x, float y ) // Constructor with parameters
+{ 
+Coefficient1 = x;
+Coefficient2 = y;
+Power1 = 1;
+Power2 = 1;
+*/
 
 /*
-int GetPower()
-  {
-   int Power [2] = {1 , 2};
-   return power;
-  }
+  if (z < 1){
+    Power1 = 1;
+    }
+    else{
+    Power1 = z;
+    }
+
+  
+
+    if (z < 1){
+    Power2 = 1;
+    }
+    else{
+    Power2 = q;
+    }
 */
+
+/*
+Binomial::Binomial(float Coefficient1, float Coefficient2 ){
+
+}*/
+
+
+ float Binomial::GetCoefficient(int x)
+   {
+      if (x == 1){
+        return  Coefficient1;
+      } 
+        
+      else if (x == 2){
+        return Coefficient2;
+      } 
+      else{
+        return -1;
+      }
+
+
+ }
+
+int Binomial::GetPower(float x)
+  {
+
+      if (x == 1){
+        return  Power1;
+      } 
+        
+      else if (x == 2){
+        return Power2;
+      } 
+      else{
+        return -1;
+      }
+}
+
+int Binomial::SetPower (int x)
+  {
+      if(x != 1){
+        Power1 = 1;
+        Power2 = 1;
+      }
+      else if(x == 1){
+        return 0;
+      }
+      else{
+        return -1;
+      }
+    }
+
+
+    int Binomial::Add (int x , int y){
+
+    }
+
+    int Binomial::Multiply (int num , int mono){
+
+    }
+
+    int Binomial::Multiply (float num , int mono){
+
+    }
+
+
--- binomial.h
+++ binomial.h
@@ -1,41 +1,103 @@
-#ifndef binomial_h
-#define binomial_h
 
-namespace N
-{
+
+
+
 class Binomial {        // The class
           // Access specifier
 
-  
+  private:
     float Coefficient1 = 1.0;  // Attribute
     float Coefficient2 = 1.0;  // Attribute
     int Power1 = 1; 
     int Power2 = 1;     // Attribute
 
 public:
-Binomial(float x, float y, int z, int q) { // Constructor with parameters
-:Coefficient1 = x;
-:Coefficient2 = y;
+//b(float x, float y, int z, int q) {
+Binomial(float x, float y ){ // Constructor with parameters
+ 
+Coefficient1 = x;
+Coefficient2 = y;
+Power1 = 1;
+Power2 = 1;
 
-
-if (z < 1){
-  Power1 = 1;
-  }
-  else{
-  Power1 = z;
-  }
+/*
+  if (z < 1){
+    Power1 = 1;
+    }
+    else{
+    Power1 = z;
+    }
 
   
 
-  if (z < 1){
-  Power2 = 1;
-  }
-  else{
-  Power2 = q;
-  }
+    if (z < 1){
+    Power2 = 1;
+    }
+    else{
+    Power2 = q;
+    }
 
-  }
-};
+*/
 }
-#endif
+
+    //}
+
+      float GetCoefficient(int x);
+      
+      /*{
+      if (x == 1){
+        return  Coefficient1;
+      } 
+        
+      else if (x == 2){
+        return Coefficient2;
+      } 
+      else{
+        return -1;
+      }
+
+    }*/
+
+
+
+    int GetPower(float x);
+    /*{
+
+      if (x == 1){
+        return  Power1;
+      } 
+        
+      else if (x == 2){
+        return Power2;
+      } 
+      else{
+        return -1;
+      }
+    }*/
+
+    int SetPower (int x);
+    /*{
+      if(x != 1){
+        Power1 = 1;
+        Power2 = 1;
+      }
+      else if{
+        return 0;
+      }
+      else{
+        return -1;
+      }
+    }*/
+
+    int Add (int x , int y);
+
+    int Multiply (int num , int mono);
+
+    int Multiply (float num , int mono);
+
+
+
+  };
+
+
 
